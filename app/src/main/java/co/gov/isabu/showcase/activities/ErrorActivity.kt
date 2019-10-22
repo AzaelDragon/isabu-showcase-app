06a8b80049e9a97c3a52125a6efb9cd8b0eb08ed
Azæl DragonFang <azael@drakonspark.tech>
2019-10-22 12:51:33 -0500

    Component (Activity): Create error Splash activity
    + Add retry button handler
    + Add logo

---
+++ app/src/main/java/co/gov/isabu/showcase/activities/ErrorActivity.kt
@@ -0,0 +1,26 @@
+package co.gov.isabu.showcase.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import co.gov.isabu.showcase.R
+
+class ErrorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+
+        val button = findViewById<Button>(R.id.error_button)
+        button.setOnClickListener {
+
+            val intent = Intent(this, SplashActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+}

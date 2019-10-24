d730150a37609fc5e60c3ab8febb4f668067992e
Azæl DragonFang <azael@drakonspark.tech>
2019-10-24 14:53:14 -0500

    Bug (Project) Add missing documentation

--- app/src/main/java/co/gov/isabu/showcase/tasks/CacheUpdateTask.kt
+++ app/src/main/java/co/gov/isabu/showcase/tasks/CacheUpdateTask.kt
@@ -7,12 +7,24 @@ import co.gov.isabu.showcase.helpers.NetworkHelper
 import co.gov.isabu.showcase.helpers.StorageHelper
 import java.lang.ref.WeakReference
 
+/**
+ * Asynchronous Worker Task for refreshing the System's Internal Storage cache.
+ */
+
 class CacheUpdateTask internal constructor(activity: AppCompatActivity) : AsyncTask<Unit, CacheUpdateTask.Progress, Unit>() {
 
     private val activityReference: WeakReference<AppCompatActivity> = WeakReference(activity)
     private lateinit var mProgressDialog: ProgressDialog
 
+    /**
+     * Simple class to create dynamic progress dialogs based on title.
+     */
+
     class Progress(val title: String, val showFlag: Boolean)
+
+    /**
+     * Instantiate dynamic progress dialog
+     */
 
     override fun onPreExecute() {
 
@@ -23,6 +35,10 @@ class CacheUpdateTask internal constructor(activity: AppCompatActivity) : AsyncT
         this.mProgressDialog.setCancelable(false)
 
     }
+
+    /**
+     * Completely downloads again the JSON resource map from the remote URL.
+     */
 
     override fun doInBackground(vararg params: Unit?) {
 
@@ -35,6 +51,11 @@ class CacheUpdateTask internal constructor(activity: AppCompatActivity) : AsyncT
 
     }
 
+    /**
+     * Refreshes the progress dialog with the provided title, and verifies if it needs to be shown
+     * in screen.
+     */
+
     override fun onProgressUpdate(vararg values: Progress?) {
 
         val progressObj = values[0]!!
@@ -42,6 +63,11 @@ class CacheUpdateTask internal constructor(activity: AppCompatActivity) : AsyncT
         if (!mProgressDialog.isShowing && progressObj.showFlag) this.mProgressDialog.show()
 
     }
+
+    /**
+     * Verifies and downloads (if required) all the required files pending update that get detected
+     * on the restart of the application.
+     */
 
     override fun onPostExecute(result: Unit?) {
 

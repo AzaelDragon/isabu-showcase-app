0c4de4780d697c8fb95ad07523004ec1a0751994
Azæl DragonFang <azael@drakonspark.tech>
2019-11-05 16:35:36 -0500

    Quality (Project): Fix empty statement block
    ~ Completely changed possible empty switch state with an if/else statement.

--- app/src/main/java/co/gov/isabu/showcase/components/CyclicTransitionDrawable.kt
+++ app/src/main/java/co/gov/isabu/showcase/components/CyclicTransitionDrawable.kt
@@ -47,30 +47,23 @@ open class CyclicTransitionDrawable(var drawables: Array<Drawable>) : LayerDrawa
 
         var done = true
 
-        when (transitionStatus) {
+        if  (transitionStatus == TransitionState.STARTING) {
 
-            TransitionState.STARTING -> {
+            done = false
+            transitionStatus = TransitionState.RUNNING
+
+        }
+        else if (transitionStatus == TransitionState.PAUSED) {
+
+            val uptimeCalc = SystemClock.uptimeMillis() - startTimeMillis
+
+            if ( uptimeCalc > pauseDuration ) {
 
                 done = false
+                startTimeMillis = SystemClock.uptimeMillis()
                 transitionStatus = TransitionState.RUNNING
 
             }
-
-            TransitionState.PAUSED -> {
-
-                val uptimeCalc = SystemClock.uptimeMillis() - startTimeMillis
-
-                if ( uptimeCalc > pauseDuration ) {
-
-                    done = false
-                    startTimeMillis = SystemClock.uptimeMillis()
-                    transitionStatus = TransitionState.RUNNING
-
-                }
-
-            }
-
-            TransitionState.RUNNING -> {}
 
         }
 
